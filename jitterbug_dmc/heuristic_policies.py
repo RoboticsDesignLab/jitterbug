d01e1d03c469ac9221d8bc543be67e80e2d6a8fc
Aaron Snoswell <aaronsnoswell@gmail.com>
2019-05-08 12:38:56 +1000

    Use same heuristic policy as task for demos

--- jitterbug_dmc/heuristic_policies.py
+++ jitterbug_dmc/heuristic_policies.py
@@ -120,13 +120,14 @@ def demo():
     # Add the jitterbug tasks to the suite
     import jitterbug_dmc
 
+    task = "move_to_pose"
     viewer.launch(
         suite.load(
             domain_name="jitterbug",
-            task_name="move_to_pose",
+            task_name=task,
             visualize_reward=True
         ),
-        policy=move_to_pose,
+        policy=eval(task),
         title="Jitterbug Policy Demo"
     )
 

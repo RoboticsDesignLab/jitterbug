6c6d23065aa1d282703fa8795b5119e9bc9dcf08
Aaron Snoswell <aaronsnoswell@gmail.com>
2019-05-08 21:33:26 +1000

    Fix JitterbugProcessor import

--- benchmarks/__init__.py
+++ benchmarks/__init__.py
@@ -1,6 +1,8 @@
 """Code to reproduce benchmarking results from the jitterbug paper"""
 
-import keras_rl_processor
+from keras_rl_processor import (
+    JitterbugProcessor
+)
 from evaluate_policy import (
     evaluate_policy,
     plot_policy_returns

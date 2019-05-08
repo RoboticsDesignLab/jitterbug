1eaed76ca4fba919ef806d784b6dc0c1bb8593cf
Aaron Snoswell <aaronsnoswell@gmail.com>
2019-05-08 12:07:21 +1000

    Update keras_rl_processor
    Support numpy array rewards and pre-flattened observation dicts

--- benchmarks/keras_rl_processor.py
+++ benchmarks/keras_rl_processor.py
@@ -1,5 +1,6 @@
 """A Keras-RL processor class for the Jitterbug domain"""
 
+import collections
 import numpy as np
 
 from rl.processors import WhiteningNormalizerProcessor
@@ -8,11 +9,18 @@ from rl.processors import WhiteningNormalizerProcessor
 class JitterbugProcessor(WhiteningNormalizerProcessor):
     """A processor to convert Jitterbug observations and actions"""
 
-    # Observation wrapper is not needed if flat_observation=True is passed to
-    # the environment constructor
-    # def process_observation(self, observation):
-    #     """Convert the Dict space observation to a flat vector"""
-    #     return np.concatenate([v for v in observation.values()], axis=0)
+    def process_observation(self, observation):
+        """Convert the Dict space observation to a flat vector"""
+        if isinstance(observation, collections.OrderedDict):
+            # Observation dict has already been flattened by dm_control
+            return observation['observations']
+        else:
+            # Manually flatten observation dict
+            return np.concatenate([v for v in observation.values()], axis=0)
+
+    def process_reward(self, reward):
+        """DeepMind Control returns numpy arrays for reward, we want float"""
+        return reward[0]
 
     def process_action(self, action):
         """Clip actions"""

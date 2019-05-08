ed71a67cfd781c5cc9c176bdc84e30dc40d3f963
Aaron Snoswell <aaronsnoswell@gmail.com>
2019-05-08 21:33:39 +1000

    Add AgentCheckpointCallback

--- benchmarks/__init__.py
+++ benchmarks/__init__.py
@@ -3,6 +3,11 @@
 from keras_rl_processor import (
     JitterbugProcessor
 )
+
+from keras_rl_agent_checkpoint_callback import (
+    AgentCheckpointCallback
+)
+
 from evaluate_policy import (
     evaluate_policy,
     plot_policy_returns

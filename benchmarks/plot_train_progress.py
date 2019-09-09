bef723eeec50df187c48dd6f999805808e70000a
Aaron Snoswell <aaronsnoswell@gmail.com>
2019-09-10 09:56:20 +1000

    Support plotting across many seeds

--- benchmarks/plot_train_progress.py
+++ benchmarks/plot_train_progress.py
@@ -1,13 +1,13 @@
 
 import sys
 import csv
+import glob
 import numpy as np
 from matplotlib import pyplot as plt
 
 
-def simpleMovingAverage(data, window, mode):
-    """Smooth the highly variable data by computing the simple moving
-    average.
+def ma(data, window, *, mode='valid'):
+    """Moving average filter
 
     Args:
         data (numpy array): The data to be smoothed
@@ -25,9 +25,8 @@ def simpleMovingAverage(data, window, mode):
     return data_smoothed
 
 
-def main(file, window):
-    """Plot model training progress"""
-
+def get_reward_from_csv(file):
+    """Extract reward vector from a monitor.csv file"""
     reward = []
     with open(file, 'rt') as f:
         data = csv.reader(f)
@@ -43,31 +42,69 @@ def main(file, window):
 
             reward.append(float(row[0]))
 
-    y = np.array(reward, dtype=float)
-    y_smoothed = simpleMovingAverage(
-        y,
-        window,
-        'valid'
-    )
-    x_smoothed = np.array(list(range(0, len(y_smoothed))), dtype=int)
-    x = np.array(range(
-        -window//2,
-        -window//2 + len(y)
+    return np.array(reward, dtype=float)
+
+
+def plot_csv_glob(fileglob, window, **kwargs):
+    """Plot performance over many seeds into the current axes
+
+    Args:
+        fileglob (str): File path glob matching one or more monitor.csv files
+        window (int): Moving average window
+
+        kwargs (dict): Plotting keyword args
+    """
+
+    files = list(glob.glob(fileglob))
+    print("Loading rewards from {} files".format(len(files)))
+    rewards = [
+        get_reward_from_csv(f)
+        for f in files
+    ]
+
+    reward_len = [
+        len(r)
+        for r in rewards
+    ]
+    min_len = min(reward_len)
+    max_len = max(reward_len)
+    print("Min training length: {}, max: {}".format(
+        min_len,
+        max_len
     ))
 
-    p0 = plt.plot(
-        x_smoothed,
-        y_smoothed
-    )
-    plt.plot(
-        x,
-        y,
-        color=p0[0].get_color(),
-        alpha=0.1,
-        lw=1
+    # Crop all rewards to the minimum length
+    rewards = np.array([
+        r[0:min_len]
+        for r in rewards
+    ], dtype=float)
+    x = np.arange(0, rewards.shape[1])
+
+    # Smooth
+    q1, q2, q3 = (
+        ma(q, window)
+        for q in np.percentile(rewards, q=[25, 50, 75], axis=0)
     )
 
-    plt.title(file)
+    x = np.arange(window // 2, window // 2 + len(q1))
+
+    p0 = plt.plot(x, q2, **kwargs)
+    plt.fill_between(
+        x,
+        q1,
+        q3,
+        color=p0[0].get_color(),
+        alpha=0.1
+    )
+
+    return rewards
+
+
+def main(fileglob, window, **kwargs):
+    """Plot model training progress"""
+    plt.figure()
+    r = plot_csv_glob(fileglob, window, **kwargs)
+    plt.title("{} ({} files)".format(fileglob, len(r)))
     plt.xlabel("1e3 timesteps")
     plt.ylabel("Reward")
     plt.ylim(-50, 1050)
@@ -79,7 +116,7 @@ def main(file, window):
 
 if __name__ == '__main__':
 
-    file = sys.argv[1]
+    fileglob = sys.argv[1]
     window = int(sys.argv[2])
 
-    main(file, window)
+    main(fileglob, window)

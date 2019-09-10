b0da44698d6ce35728ac4d3faf4a716fa02b8eae
Aaron Snoswell <aaronsnoswell@gmail.com>
2019-09-10 14:17:21 +1000

    Fix benchmark inports

--- benchmarks/__init__.py
+++ benchmarks/__init__.py
@@ -1,11 +1,15 @@
 """Code to reproduce benchmarking results from the jitterbug paper"""
 
-from keras_rl_helpers import (
-    JitterbugProcessor,
-    AgentCheckpointCallback
-)
+# from benchmarks.keras_rl_helpers import (
+#     JitterbugProcessor,
+#     AgentCheckpointCallback
+# )
+#
+# from benchmarks.evaluate_policy import (
+#     evaluate_policy,
+#     plot_policy_returns
+# )
 
-from evaluate_policy import (
-    evaluate_policy,
-    plot_policy_returns
+from benchmarks.plot_train_progress import (
+    plot_csv_glob
 )
